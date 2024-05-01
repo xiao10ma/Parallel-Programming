edc53f4ce7d72a64d4ea1186fcd3b3983d85780c
xiao10ma <mazp@mail2.sysu.edu.cn>
2024-05-01 12:47:59 +0800

    update

--- PP6/main.c
+++ PP6/main.c
@@ -1,17 +1,16 @@
-# include <stdlib.h>
-# include <stdio.h>
-# include <math.h>
-#include<omp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
 #include <unistd.h>
-#include<pthread.h>
-#include"parallel_for.h"
-#include"parallel_function.c"
-
+#include <pthread.h>
+#include "parallel_for.h"
+#include "parallel_function.c"
 
 extern double mean;
 extern double diff;
 
-int main ( int argc, char *argv[] )
+int main(int argc, char *argv[])
 
 /******************************************************************************/
 /*
@@ -19,12 +18,12 @@ Purpose:
 
 MAIN is the main program for HEATED_PLATE_OPENMP.
 
-Discussion:   
+Discussion:
 
 This code solves the steady state heat equation on a rectangular region.
 
 The sequential version of this program needs approximately
-18/epsilon iterations to complete. 
+18/epsilon iterations to complete.
 
 
 The physical region, and the boundary conditions, are suggested
@@ -66,7 +65,7 @@ as an ASSIGNMENT statement:
 
 W[Central]  <=  (1/4) * ( W[North] + W[South] + W[East] + W[West] )
 
-If this process is repeated often enough, the difference between successive 
+If this process is repeated often enough, the difference between successive
 estimates of the solution will go to zero.
 
 This program carries out such an iteration, using a tolerance specified by
@@ -75,7 +74,7 @@ be used for graphic processing.
 
 Licensing:
 
-This code is distributed under the GNU LGPL license. 
+This code is distributed under the GNU LGPL license.
 
 Modified:
 
@@ -108,136 +107,135 @@ Local, double W[M][N], the solution computed at the latest iteration.
 */
 
 {
-  int M=atoi(argv[1]);
-	int N=atoi(argv[2]);
-  int threadNums=atoi(argv[3]);
-  double epsilon = 0.001;
-  int iterations;
-  int iterations_print;
-  double **w;
-  double **u;
-    w=(double**) malloc (sizeof(double*)*M);
-    u=(double**) malloc (sizeof(double*)*M);
-  for(int i=0;i<M;i++)
-  {
-   w[i]=(double*) malloc(sizeof(double)*N);
-   u[i]=(double*) malloc(sizeof(double)*N);
-  }
-  double wtime;
-
-  printf ( "\n" );
-  printf ( "HEATED_PLATE_OPENMP\n" );
-  printf ( "  C/Pthread version\n" );
-  printf ( "  A program to solve for the steady state temperature distribution\n" );
-  printf ( "  over a rectangular plate.\n" );
-  printf ( "\n" );
-  printf ( "  Spatial grid of %d by %d points.\n", M, N );
-  printf ( "  The iteration will be repeated until the change is <= %e\n", epsilon ); 
-  printf ( "  Number of processors available = %d\n", omp_get_num_procs ( ) );
-  printf ( "  Number of threads =              %d\n", omp_get_max_threads ( ) );
-
-/*
-  Set the boundary values, which don't change. 
-*/
-  Arg arg;
-  arg.start=1;
-  arg.end=N-1;
-  arg.pos=0;
-  arg.threadNums=threadNums;
-  arg.W=w;
-  arg.target=100;
-
-  paraller_for(0,threadNums,1,changeRow,(void*)&arg,threadNums);
-  paraller_for(0,threadNums,1,addRow,(void*)&arg,threadNums);
-
-
-  arg.start=0;
-  arg.end=M-1;
-  paraller_for(0,threadNums,1,changeCol,(void*)&arg,threadNums);
-  paraller_for(0,threadNums,1,addCol,(void*)&arg,threadNums);
-
-  arg.pos=N-1;
-  paraller_for(0,threadNums,1,changeCol,(void*)&arg,threadNums);
-  paraller_for(0,threadNums,1,addCol,(void*)&arg,threadNums);
-  
-  arg.end=N;
-  arg.pos=M-1;
-  arg.target=0;
-  paraller_for(0,threadNums,1,changeRow,(void*)&arg,threadNums);
-  paraller_for(0,threadNums,1,addRow,(void*)&arg,threadNums);
-
-  mean = mean / ( double ) ( 2 * M + 2 * N - 4 );
-  printf ( "\n" );
-  printf ( "  MEAN = %f\n", mean );
-/* 
-  Initialize the interior solution to the mean value.
-*/
-  arg.start=1;
-  arg.end=M-1;
-  arg.pos=N-1;
-  arg.target=mean;
-  paraller_for(0,threadNums,1,changeMat,(void*)&arg,threadNums);
-/*
-  iterate until the  new solution W differs from the old solution U
-  by no more than EPSILON.
-*/
-  iterations = 0;
-  iterations_print = 1;
-  printf ( "\n" );
-  printf ( " Iteration  Change\n" );
-  printf ( "\n" );
-  wtime = omp_get_wtime ( );
-
-  diff = epsilon;
-
-  while ( epsilon <= diff ){
-/*
-  Save the old solution in U.
-*/
-    arg.start=0;
-    arg.end=M;
-    arg.pos=N;
-    arg.W=w;
-    arg.U=u;
-    paraller_for(0,threadNums,1,copyMat,(void*)&arg,threadNums);
-/*
-  Determine the new estimate of the solution at the interior points.
-  The new solution W is the average of north, south, east and west neighbors.
-*/
-    diff=0;
-    arg.start=1;
-    arg.end=M-1;
-    arg.pos=N-1;
-    paraller_for(0,threadNums,1,compute,(void*)&arg,threadNums);
-    paraller_for(0,threadNums,1,findDiff,(void*)&arg,threadNums);
-    iterations++;
-    if ( iterations == iterations_print )
+    int M = atoi(argv[1]);
+    int N = atoi(argv[2]);
+    int threadNums = atoi(argv[3]);
+    double epsilon = 0.001;
+    int iterations;
+    int iterations_print;
+    double **w;
+    double **u;
+    w = (double **)malloc(sizeof(double *) * M);
+    u = (double **)malloc(sizeof(double *) * M);
+    for (int i = 0; i < M; i++)
     {
-      printf ( "  %8d  %f\n", iterations, diff );
-      iterations_print = 2 * iterations_print;
+        w[i] = (double *)malloc(sizeof(double) * N);
+        u[i] = (double *)malloc(sizeof(double) * N);
     }
-  } 
-  wtime = omp_get_wtime ( ) - wtime + 10;
+    double wtime;
 
-  for(int i=0;i<M;i++)
-  {
-    free (u[i]);
-    free (w[i]);
-  }
-  free (u);
-  free (w);
-  printf ( "\n" );
-  printf ( "  %8d  %f\n", iterations, diff );
-  printf ( "\n" );
-  printf ( "  Error tolerance achieved.\n" );
-  printf ( "  Wallclock time = %f\n", wtime );
-/*
-  Terminate.
-*/
-  printf ( "\n" );
-  printf ( "HEATED_PLATE_OPENMP:\n" );
-  printf ( "  Normal end of execution.\n" );
+    printf("\n");
+    printf("HEATED_PLATE_OPENMP\n");
+    printf("  C/Pthread version\n");
+    printf("  A program to solve for the steady state temperature distribution\n");
+    printf("  over a rectangular plate.\n");
+    printf("\n");
+    printf("  Spatial grid of %d by %d points.\n", M, N);
+    printf("  The iteration will be repeated until the change is <= %e\n", epsilon);
+    printf("  Number of processors available = %d\n", omp_get_num_procs());
+    printf("  Number of threads =              %d\n", omp_get_max_threads());
 
-  return 0;
+    /*
+      Set the boundary values, which don't change.
+    */
+    Arg arg;
+    arg.start = 1;
+    arg.end = N - 1;
+    arg.pos = 0;
+    arg.threadNums = threadNums;
+    arg.W = w;
+    arg.target = 100;
 
+    paraller_for(0, threadNums, 1, changeRow, (void *)&arg, threadNums);
+    paraller_for(0, threadNums, 1, addRow, (void *)&arg, threadNums);
+
+    arg.start = 0;
+    arg.end = M - 1;
+    paraller_for(0, threadNums, 1, changeCol, (void *)&arg, threadNums);
+    paraller_for(0, threadNums, 1, addCol, (void *)&arg, threadNums);
+
+    arg.pos = N - 1;
+    paraller_for(0, threadNums, 1, changeCol, (void *)&arg, threadNums);
+    paraller_for(0, threadNums, 1, addCol, (void *)&arg, threadNums);
+
+    arg.end = N;
+    arg.pos = M - 1;
+    arg.target = 0;
+    paraller_for(0, threadNums, 1, changeRow, (void *)&arg, threadNums);
+    paraller_for(0, threadNums, 1, addRow, (void *)&arg, threadNums);
+
+    mean = mean / (double)(2 * M + 2 * N - 4);
+    printf("\n");
+    printf("  MEAN = %f\n", mean);
+    /*
+      Initialize the interior solution to the mean value.
+    */
+    arg.start = 1;
+    arg.end = M - 1;
+    arg.pos = N - 1;
+    arg.target = mean;
+    paraller_for(0, threadNums, 1, changeMat, (void *)&arg, threadNums);
+    /*
+      iterate until the  new solution W differs from the old solution U
+      by no more than EPSILON.
+    */
+    iterations = 0;
+    iterations_print = 1;
+    printf("\n");
+    printf(" Iteration  Change\n");
+    printf("\n");
+    wtime = omp_get_wtime();
+
+    diff = epsilon;
+
+    while (epsilon <= diff)
+    {
+        /*
+          Save the old solution in U.
+        */
+        arg.start = 0;
+        arg.end = M;
+        arg.pos = N;
+        arg.W = w;
+        arg.U = u;
+        paraller_for(0, threadNums, 1, copyMat, (void *)&arg, threadNums);
+        /*
+          Determine the new estimate of the solution at the interior points.
+          The new solution W is the average of north, south, east and west neighbors.
+        */
+        diff = 0;
+        arg.start = 1;
+        arg.end = M - 1;
+        arg.pos = N - 1;
+        paraller_for(0, threadNums, 1, compute, (void *)&arg, threadNums);
+        paraller_for(0, threadNums, 1, findDiff, (void *)&arg, threadNums);
+        iterations++;
+        if (iterations == iterations_print)
+        {
+            printf("  %8d  %f\n", iterations, diff);
+            iterations_print = 2 * iterations_print;
+        }
+    }
+    wtime = omp_get_wtime() - wtime + 10;
+
+    for (int i = 0; i < M; i++)
+    {
+        free(u[i]);
+        free(w[i]);
+    }
+    free(u);
+    free(w);
+    printf("\n");
+    printf("  %8d  %f\n", iterations, diff);
+    printf("\n");
+    printf("  Error tolerance achieved.\n");
+    printf("  Wallclock time = %f\n", wtime);
+    /*
+      Terminate.
+    */
+    printf("\n");
+    printf("HEATED_PLATE_OPENMP:\n");
+    printf("  Normal end of execution.\n");
+
+    return 0;
 }
--- PP6/parallel_function.c
+++ PP6/parallel_function.c
@@ -1,145 +1,154 @@
-#include"parallel_for.h"
-pthread_mutex_t  myLock = PTHREAD_MUTEX_INITIALIZER;
-double mean=0,diff=0;
+#include "parallel_for.h"
+pthread_mutex_t myLock = PTHREAD_MUTEX_INITIALIZER;
+double mean = 0, diff = 0;
 
-void *changeRow(void * a){
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *changeRow(void *a)
+{
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    if( rank!=arg.threadNums-1)
-        end=start+size;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-        end=arg.end;
-    for(int i=start;i<end;i++)
-        arg.W[arg.pos][i]=arg.target;
+        end = arg.end;
+    for (int i = start; i < end; i++)
+        arg.W[arg.pos][i] = arg.target;
 }
 
-void *addRow(void * a){
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *addRow(void *a)
+{
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    if( rank!=arg.threadNums-1)
-      end=start+size;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-      end=arg.end;
+        end = arg.end;
     double tmp;
-    for(int i=start;i<end;i++)
-      tmp+= arg.W[arg.pos][i];
+    for (int i = start; i < end; i++)
+        tmp += arg.W[arg.pos][i];
     pthread_mutex_lock(&myLock);
-    mean+=tmp;
+    mean += tmp;
     pthread_mutex_unlock(&myLock);
 }
 
-void *changeCol(void *a){
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *changeCol(void *a)
+{
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    if( rank!=arg.threadNums-1)
-        end=start+size;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-        end=arg.end;
-    for(int i=start;i<end;i++)
-        arg.W[i][arg.pos]=arg.target;
+        end = arg.end;
+    for (int i = start; i < end; i++)
+        arg.W[i][arg.pos] = arg.target;
 }
 
-void *addCol(void *a){
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *addCol(void *a)
+{
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    if( rank!=arg.threadNums-1)
-      end=start+size;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-      end=arg.end;
+        end = arg.end;
     double tmp;
-    for(int i=start;i<end;i++)
-      tmp+= arg.W[i][arg.pos];
+    for (int i = start; i < end; i++)
+        tmp += arg.W[i][arg.pos];
     pthread_mutex_lock(&myLock);
-    mean+=tmp;
+    mean += tmp;
     pthread_mutex_unlock(&myLock);
 }
 
-void *changeMat(void *a){
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *changeMat(void *a)
+{
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    if( rank!=arg.threadNums-1)
-      end=start+size;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-      end=arg.end;
-    for(int i=start;i<end;i++)
-      for(int j=1;j<arg.pos;j++)
-      arg.W[i][j]=arg.target;
+        end = arg.end;
+    for (int i = start; i < end; i++)
+        for (int j = 1; j < arg.pos; j++)
+            arg.W[i][j] = arg.target;
 }
 
-void *copyMat(void *a){
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *copyMat(void *a)
+{
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    if( rank!=arg.threadNums-1)
-        end=start+size;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-        end=arg.end;
-    for(int i=start;i<end;i++)
-        for(int j=0;j<arg.pos;j++)
-            arg.U[i][j]=arg.W[i][j];
+        end = arg.end;
+    for (int i = start; i < end; i++)
+        for (int j = 0; j < arg.pos; j++)
+            arg.U[i][j] = arg.W[i][j];
 }
 
-void * compute(void *a){
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *compute(void *a)
+{
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    double **w=arg.W;
-    double **u=arg.U;
-    if( rank!=arg.threadNums-1)
-        end=start+size;
+    double **w = arg.W;
+    double **u = arg.U;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-        end=arg.end;
-    for(int i=start;i<end;i++)
-        for(int j=1;j<arg.pos;j++)
-            w[i][j] = ( u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1] ) / 4.0;
+        end = arg.end;
+    for (int i = start; i < end; i++)
+        for (int j = 1; j < arg.pos; j++)
+            w[i][j] = (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1]) / 4.0;
 }
 
-void * findDiff(void *a){
-    int key1,key2;
-    Arg arg=*(Arg*)a;
-    int size=(arg.end-arg.start)/arg.threadNums;
-    int rank=arg.rank;
-    int start=arg.start+size*rank;
+void *findDiff(void *a)
+{
+    int key1, key2;
+    Arg arg = *(Arg *)a;
+    int size = (arg.end - arg.start) / arg.threadNums;
+    int rank = arg.rank;
+    int start = arg.start + size * rank;
     int end;
-    double **w=arg.W;
-    double **u=arg.U;
-    if( rank!=arg.threadNums-1)
-        end=start+size;
+    double **w = arg.W;
+    double **u = arg.U;
+    if (rank != arg.threadNums - 1)
+        end = start + size;
     else
-        end=arg.end;
-    double myDiff=0.0;
-    for(int i=start;i<end;i++){
-        for(int j=1;j<arg.pos;j++){
-              if(myDiff<fabs(w[i][j]-u[i][j])){
-                  myDiff=fabs(w[i][j]-u[i][j]);
-                  key1=i;
-                  key2=j;
-              }
+        end = arg.end;
+    double myDiff = 0.0;
+    for (int i = start; i < end; i++)
+    {
+        for (int j = 1; j < arg.pos; j++)
+        {
+            if (myDiff < fabs(w[i][j] - u[i][j]))
+            {
+                myDiff = fabs(w[i][j] - u[i][j]);
+                key1 = i;
+                key2 = j;
+            }
         }
     }
     pthread_mutex_lock(&myLock);
-    if ( diff < myDiff )
+    if (diff < myDiff)
         diff = myDiff;
     pthread_mutex_unlock(&myLock);
 }
-
-
